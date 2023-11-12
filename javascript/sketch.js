c98298b42a3f21af86c3e5d48190b17998fd00f2
ievavaiti <146743152+ieva-gsmd@users.noreply.github.com>
2023-11-12 18:19:43 +0000

    init

---
+++ javascript/sketch.js
@@ -0,0 +1,50 @@
+let synth;
+let playing, note;
+
+function setup() {
+
+cnv = createCanvas(windowWidth, windowHeight);
+cnv.mousePressed(playSynth);
+
+const lfilter = new Tone.Filter({
+  frequency: 1200,
+  type: 'lowpass',
+  rolloff: -48
+}).toDestination();
+
+synth = new Tone.Synth({
+  type: "square",
+  volume: -6
+}).connect(lfilter);
+
+
+const reverb = new Tone.Reverb({
+  wet: 0.5,
+  decay: 5
+}).toDestination();
+
+lfilter.connect(reverb);
+
+}
+
+function draw() {
+  background(220);
+
+  note = map(mouseY, 0, height, 20, 1000);
+  
+  if (playing) {
+    
+    synth.setNote(note, 0.1);
+  }
+
+}
+
+function playSynth() {
+  synth.triggerAttack("C4", 0.3);
+  playing = true;
+}
+
+function mouseReleased() {
+  synth.triggerRelease("+0.2")
+  playing = false;
+}
